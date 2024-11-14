378536fc4a5072c019d2b87316c63ae3b041dc8a
Leon <leon@reusch-home.de>
2024-11-14 22:08:45 +0100

    add files

---
+++ src/main/java/com/example/services/App.java
@@ -0,0 +1,14 @@
+package com.example.services;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+}
